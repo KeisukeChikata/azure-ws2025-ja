56dab140ce9e0217e25c4f3cdf46db8009b06fd0
Keisuke Chikata <147702488+KeisukeChikata@users.noreply.github.com>
2025-05-26 15:01:17 +0900

    Update Set-Japanese.ps1

--- scripts/Set-Japanese.ps1
+++ scripts/Set-Japanese.ps1
@@ -1,5 +1,13 @@
+# 日本語言語パックの追加
+Add-WindowsCapability -Online -Name Language.Basic~~~ja-JP~0.0.1.0
+
+# 表示言語とユーザー言語設定
 Set-WinSystemLocale ja-JP
-Set-WinUserLanguageList -LanguageList ja-JP -Force
+Set-WinUserLanguageList ja-JP -Force
 Set-Culture ja-JP
+Set-WinUILanguageOverride -Language ja-JP
 Set-WinHomeLocation -GeoId 122
 Set-TimeZone -Id "Tokyo Standard Time"
+
+# 再起動で反映
+Restart-Computer
