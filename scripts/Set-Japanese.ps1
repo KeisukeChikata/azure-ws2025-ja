953b420b3f1d2ee000bcf1a2dfe6268e2b9b7e41
Keisuke Chikata <147702488+KeisukeChikata@users.noreply.github.com>
2025-05-26 17:38:07 +0900

    Update Set-Japanese.ps1

--- scripts/Set-Japanese.ps1
+++ scripts/Set-Japanese.ps1
@@ -1,8 +1,5 @@
 # 日本語の言語パックをインストール（UIも含める）
 Add-WindowsCapability -Online -Name "Language.Basic~~~ja-JP~0.0.1.0"
-Add-WindowsCapability -Online -Name "Language.Handwriting~~~ja-JP~0.0.1.0"
-Add-WindowsCapability -Online -Name "Language.Speech~~~ja-JP~0.0.1.0"
-Add-WindowsCapability -Online -Name "Language.TextToSpeech~~~ja-JP~0.0.1.0"
 
 # 優先する言語設定を日本語最優先を変更
 Set-WinUserLanguageList -LanguageList ja-JP,en-US -Force
