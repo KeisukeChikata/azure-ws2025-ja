37b6eb325845fef0317151a1cb3a41b550f5531d
Keisuke Chikata <147702488+KeisukeChikata@users.noreply.github.com>
2025-05-29 09:57:06 +0900

    Update Set-Japanese.ps1

--- scripts/Set-Japanese.ps1
+++ scripts/Set-Japanese.ps1
@@ -1,7 +1,7 @@
 # 日本語のLanguageパックをインストール
 Install-Language ja-JP
 
-# 優先する言語設定を日本語最優先を変更
+# 優先する言語設定を日本語最優先に変更
 Set-WinUserLanguageList -LanguageList ja-JP,en-US -Force
 
 # システムUI表示言語を日本語に変更
@@ -11,7 +11,7 @@ Set-SystemPreferredUILanguage ja-JP
 Set-WinHomeLocation -GeoId 122
 
 # タイムゾーンを日本時間に変更
-Set-TimeZone -id “Tokyo Standard Time"
+Set-TimeZone -id "Tokyo Standard Time"
 
 # システムロケール設定を日本に変更
 Set-WinSystemLocale ja-JP
@@ -19,5 +19,4 @@ Set-WinSystemLocale ja-JP
 # ようこそ画面と新しいユーザーアカウント設定をコピー
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 
-# Windows OSを再起動
-Restart-Computer
+# Restart-Computer は不要または別途実行
